4152073645da1feb606813b0aa67da2eb736e4fc
zhaminglei <minglei@Mingleis-MacBook-Pro.local>
2015-01-26 23:42:25 -0800

    adding code of calling server with ajax

--- src/app/Ajax.js
+++ src/app/Ajax.js
@@ -1,20 +1,33 @@
 (function(global){
+
+	global.NameSpaces = {};
+	global.NameSpaces.MedicineManagement = {};
+
+	var urlAllMedicines = "http://54.69.194.178:9000/medicines";
+
 	var GetMedicinesMock = function (){
-		return [{
-			name:"amoxilin",
-			price:10,
-			feature:"children can eat"
-		},
+		var medicines = [];
+
+		for(var i =0;i<10;i++)
 		{
+			medicines.push({
 			name:"amoxilin",
-			price:10,
-			feature:"children can eat"
-		},
-		{
-			name:"amoxilin",
-			price:10,
-			feature:"children can eat"
-		}];
+			price:" price:10",
+			feature:"children can eat, children can eat, children can eat, children can eat, children can eat, children can eat, children can eat, children can eat, children can eat "
+			});
+		}
+		return medicines;
 	}
-	global.GetMedicines = GetMedicinesMock;
+	
+	var _getAllMedicines = function ()
+	{
+		
+      return $.ajax({
+            url: urlAllMedicines,
+            async: false
+        }).promise();
+	} 
+
+	global.NameSpaces.MedicineManagement.GetMedicines = _getAllMedicines;
+
 })(this);
--- src/components/home-page/home.js
+++ src/components/home-page/home.js
@@ -1,15 +1,18 @@
 (function(global){
-  define(["knockout", "text!./home.html"], function(ko, homeTemplate) {
-    function HomeViewModel(route) {
-    	var self = this;
-      self.message = ko.observable('Welcome to MedincineManagement!');
-      self.medicineList = global.GetMedicines();
-  }
-    HomeViewModel.prototype.doSomething = function() {
-      this.message('You invoked doSomething() on the viewmodel.');
-    };
+    define(["knockout", "text!./home.html"], function(ko, homeTemplate) {
+        function HomeViewModel(route) {
+    	      var self = this;
+            self.medicineList = [];
+            self.message = ko.observable('Welcome to MedincineManagement!');
+            global.NameSpaces.MedicineManagement.GetMedicines().done( function (data){
+                self.medicineList = data;
+            });
+        }
 
-    return { viewModel: HomeViewModel, template: homeTemplate };
+        HomeViewModel.prototype.doSomething = function() {
+            this.message('You invoked doSomething() on the viewmodel.');
+        };
 
-  });
+        return { viewModel: HomeViewModel, template: homeTemplate };
+    });
 })(this);
